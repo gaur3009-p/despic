7c2e95d5ea3053d1c6c1038c62913803ebe20bac
gaur3009-p <gaur3009@gmail.com>
2026-03-07 19:18:37 +0530

    Update translate.py

--- services/translate.py
+++ services/translate.py
@@ -8,11 +8,17 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 def translate(text, target_lang):
 
+    # tokenize input text
     inputs = tokenizer(text, return_tensors="pt")
 
+    # get language token id correctly
+    target_lang_id = tokenizer.convert_tokens_to_ids(target_lang)
+
+    # generate translation
     tokens = model.generate(
         **inputs,
-        forced_bos_token_id=tokenizer.lang_code_to_id[target_lang]
+        forced_bos_token_id=target_lang_id
     )
 
+    # decode result
     return tokenizer.decode(tokens[0], skip_special_tokens=True)
