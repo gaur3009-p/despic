95e841247e1d72a1806fd4589b5fabdf40e4f6aa
gaur3009-p <gaur3009@gmail.com>
2026-03-09 20:32:10 +0530

    Update and rename tts_client.py to tts.py

---
+++ app/services/tts.py
@@ -0,0 +1,50 @@
+import torch
+import uuid
+import soundfile as sf
+
+from parler_tts import ParlerTTSForConditionalGeneration
+from transformers import AutoTokenizer
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = ParlerTTSForConditionalGeneration.from_pretrained(
+    "ai4bharat/indic-parler-tts"
+).to(device)
+
+tokenizer = AutoTokenizer.from_pretrained("ai4bharat/indic-parler-tts")
+
+description_tokenizer = AutoTokenizer.from_pretrained(
+    model.config.text_encoder._name_or_path
+)
+
+def generate_speech(text):
+
+    description = (
+        "Divya speaks with a clear voice at a moderate pace. "
+        "The recording is very clear audio with no background noise."
+    )
+
+    description_inputs = description_tokenizer(
+        description,
+        return_tensors="pt"
+    ).to(device)
+
+    prompt_inputs = tokenizer(
+        text,
+        return_tensors="pt"
+    ).to(device)
+
+    generation = model.generate(
+        input_ids=description_inputs.input_ids,
+        attention_mask=description_inputs.attention_mask,
+        prompt_input_ids=prompt_inputs.input_ids,
+        prompt_attention_mask=prompt_inputs.attention_mask
+    )
+
+    audio = generation.cpu().numpy().squeeze()
+
+    file = f"output_{uuid.uuid4()}.wav"
+
+    sf.write(file, audio, model.config.sampling_rate)
+
+    return file
