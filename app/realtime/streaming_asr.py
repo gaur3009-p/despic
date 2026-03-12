587ceb3725688541e2f4049feb8c205c02799f7a
gaur3009-p <gaur3009@gmail.com>
2026-03-12 16:20:14 +0530

    Create streaming_asr.py

---
+++ app/realtime/streaming_asr.py
@@ -0,0 +1,40 @@
+from faster_whisper import WhisperModel
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = WhisperModel(
+    "medium",
+    device=device,
+    compute_type="float16"
+)
+
+
+class StreamingASR:
+
+    def __init__(self):
+
+        self.last_text = ""
+
+    def transcribe(self, audio_path):
+
+        segments, info = model.transcribe(
+            audio_path,
+            beam_size=3,
+            best_of=3,
+            condition_on_previous_text=False
+        )
+
+        text = ""
+
+        for segment in segments:
+            text += segment.text
+
+        text = text.strip()
+
+        if text == self.last_text:
+            return None, info.language
+
+        self.last_text = text
+
+        return text, info.language
