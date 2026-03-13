6c40d1a3b5b13befa009eea1a7c7f6699bd1038c
gaur3009-p <gaur3009@gmail.com>
2026-03-13 08:21:59 +0530

    Update vad.py

--- app/realtime/vad.py
+++ app/realtime/vad.py
@@ -1,15 +1,14 @@
-import torch
 from silero_vad import load_silero_vad, get_speech_timestamps
 
 model = load_silero_vad()
 
 
-def get_speech_segments(audio, sample_rate):
+def detect_speech(audio, sample_rate):
 
-    speech_timestamps = get_speech_timestamps(
+    timestamps = get_speech_timestamps(
         audio,
         model,
         sampling_rate=sample_rate
     )
 
-    return speech_timestamps
+    return timestamps
