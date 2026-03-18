ee49d6294c10a911143eecf6cf415eda8bfbc82f
gaur3009-p <gaur3009@gmail.com>
2026-03-18 13:17:15 +0530

    Update asr_travel.py

--- app/services/asr_travel.py
+++ app/services/asr_travel.py
@@ -1,9 +1,7 @@
 from faster_whisper import WhisperModel
 import torch
 
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
 
 model = WhisperModel(
     "medium",
@@ -12,34 +10,21 @@ model = WhisperModel(
 )
 
 
-def transcribe_travel(audio_path):
+def transcribe_travel(audio_np):
 
     segments, info = model.transcribe(
-        audio_path,
-
-        # decoding
+        audio_np,
         beam_size=5,
         best_of=5,
-
-        # robustness
         temperature=[0.0, 0.2, 0.4],
-
-        # reduce hallucination
         no_speech_threshold=0.6,
         compression_ratio_threshold=2.4,
-
-        # speech specific
         vad_filter=True,
-
-        # faster for short audio
         condition_on_previous_text=False,
-
-        # better conversational context
         initial_prompt="This is a conversation."
     )
 
     text = ""
-
     for seg in segments:
         text += seg.text
 
